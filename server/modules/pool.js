8bcbf753395f6b362f518c3adc89d5a72e7a8f15
DeWitt Kane <dewitt.kane@gmail.com>
2020-08-07 16:41:48 -0500

    DB linked and pool module setup

---
+++ server/modules/pool.js
@@ -0,0 +1,18 @@
+const pg = require('pg');
+//requires pg into the file
+
+const pool = new pg.Pool({
+    database: 'to_do_list',
+    host: 'localhost',
+    port: 5432
+});//constructs a new pool that links to our Postgres DB
+
+pool.on("connect", () => {
+    console.log('connected to postgres!');
+});//log for successful DB connection
+
+pool.on("error", (err) => {
+    console.log('error connecting to postgres:', err);
+});// log for unsuccessful DB connection
+
+module.exports = pool;
---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(bodyParser.urlencoded( {extended:true} ));
+app.use(express.static('server/public'));
+app.listen(PORT, () => {
+    console.log('listening on port', PORT);
+  });
+//server boilerplate
+
+const listRouter = require('./routes/list.router.js')
+app.use('/   ROUTE      ', listRouter)
