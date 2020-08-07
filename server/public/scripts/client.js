91f87fb78f46cc886d2d92ba8b5d616e0ea7395a
DeWitt Kane <dewitt.kane@gmail.com>
2020-08-07 16:28:42 -0500

    linked files

---
+++ server/public/scripts/client.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    getTodoList();
+    //populates dom on page load
+
+
+    //click listeners!
+
+});
+
+function getTodoList(){
+    console.log('Getting Todo List from DB!');
+    
+}
