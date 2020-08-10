9fca20f374e21ffb4c7cc133abb10150539b31e3
DeWitt Kane <dewitt.kane@gmail.com>
2020-08-10 17:29:01 -0500

    heroku config added

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,11 +1,30 @@
 const pg = require( 'pg' );
+const url = require('url');
 //requires pg into the file
 
-const pool = new pg.Pool({
-    database: 'weekend_to_do_app',
-    host: 'localhost',
-    port: 5432
-});//constructs a new pool that links to our Postgres DB
+let config = {};
+
+if (process.env.DATABASE_URL) {
+    const params = url.parse(process.env.DATABASE_URL);
+    const auth = params.auth.split(':');
+
+    config = {
+        user: auth[0],
+        password: auth[1],
+        host: params.hostname,
+        port: params.port,
+        database: params.pathname.split('/')[1],
+        ssl: true
+    }
+    
+} else {
+    config = {
+        database: 'weekend_to_do_app',
+        host: 'localhost',
+        port: 5432
+    };
+}
+const pool = new pg.Pool(config);//constructs a new pool that links to our Postgres DB
 
 pool.on( 'connect', () => {
     console.log( 'connected to postgres!' );
